09f0f2e58168095d669263425d2c51b9e9c5f429
Muazzam Imtiaz <muazzam@rykit.com>
2023-11-18 18:03:48 +0900

    first commit

---
+++ jet-form-phone-field.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: JetFormBuilder - Phone Field Enhancement
+ * Plugin URI: 
+ * Description: Enhance phone field to have international prefix.
+ * Version: 1.0.0
+ * Author: Muazzam Imtiaz
+ * Author URI: https://muazzam.dev
+ * Text Domain: jet-form-phone-field
+ * License: GPL-3.0+
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.txt
+ * Domain Path: /languages
+ */
+
+ 
